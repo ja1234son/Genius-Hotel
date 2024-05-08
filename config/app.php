c33818480aece2ef22493ae56d198043f0811fef
ja1234son <mbaga0345@gmail.com>
2024-05-08 11:19:26 +0300

    Spatie

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
